fb8d1c773a9e777a7baa0866cd868129761267fd
panyuvila <73607368+panyuvila@users.noreply.github.com>
2022-02-19 21:23:20 +0100

    hasta ejercicio diez bikram
    fundamentos-de-programacion-bikram-var-bucles-condicionales

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-let variableSinValor
-let booleano1= booleano
-let booleano2= boolean
+let variableSinValor()
+}
+let booleano1 = 10 > 9
+}
+let ,booleano2 = 1 > 9
+}
 const PI = 3.14
 const tau = 2*PI
-const booleanoAnd = booleano1 && booleano2
+}
+const booleanoy = (booleano1 != booleano2')
+}
+let contarHasta10_2  = 0
+for ( ; contarHasta10_2 < 10)
+console log (contarHasta10_2)
+contarHasta10_2 = contarHasta10_2 + 1
+}
+let postI  = 0
+let postJ = 0
+for ( ;posti + postj < 10)
+postj++= postI + postJ
+console log (contarHasta10_2)
+}
+
